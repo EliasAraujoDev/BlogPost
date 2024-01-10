70b6c9fa95f2800411a41c4675b22277c804f914
And <144910645+andressacamile@users.noreply.github.com>
2024-01-10 09:49:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+
+app.route('/login')
+def login():
+    
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

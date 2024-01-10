4be972f8e55bd2c4e55f1cc8fa16a81dae72dac0
EliasAraujoDev <eliasaraujo1222@outlook.com>
2024-01-10 09:55:53 -0300

    rota

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-app.route('/login')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
 def login():
-    
     return render_template('login.html')
 
 
